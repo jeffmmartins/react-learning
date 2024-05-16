012072c950f26fcff9a5de68a3074d4d0714a281
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-05-16 18:40:13 -0300

    finalizado

--- usenavigate/src/App.jsx
+++ usenavigate/src/App.jsx
@@ -1,21 +1,20 @@
-import { BrowserRouter ,  Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './components/Home';
-import Sobre from './components/Sobre';
-import Header from './components/Header';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Sobre from "./components/Sobre";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-    
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Header/>}>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/sobre" element={<Sobre/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/sobre" element={<Sobre />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
