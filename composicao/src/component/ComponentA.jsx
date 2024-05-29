3d3145a3b0ccd86741c4253f9a771461d69dee8e
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-05-29 12:10:30 -0300

    exemplo composicao

---
+++ composicao/src/component/ComponentA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ComponentA = ({component}) => {
+  return (
+    <div>
+      <h3>Titulo</h3>
+      {component}
+    </div>
+  )
+}
+
+export default ComponentA
---
+++ composicao/src/component/ComponentB.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ComponentB = ({example}) => {
+  return (
+    <div>
+      <h4>Titulo exemplo</h4>
+      <p>{example}</p>
+    </div>
+  )
+}
+
+export default ComponentB
