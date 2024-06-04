0c592cf079225cbdd754fada5ae500dc587a83a1
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-06-04 18:27:54 -0300

    atualização botão adicionar e subtrair

--- propdrilling-context/src/component/CounterControls/index.jsx
+++ propdrilling-context/src/component/CounterControls/index.jsx
@@ -6,9 +6,9 @@ const CounterControls = () => {
     const [numberInput, setNumberInput] = useState("")
   return (
     <div>
-      <input type="text" value={numberInput} onChange={(e) => setNumberInput(e.target.value)}/>
-      <button onClick={adicionar(Number(numberInput))}>+</button>
-      <button onClick={subtrair(Number(numberInput))}>-</button>
+      <input type="number" value={numberInput} onChange={(e) => setNumberInput(e.target.value)}/>
+      <button onClick={() => adicionar(Number(numberInput))}>+</button>
+      <button onClick={() => subtrair(Number(numberInput))}>-</button>
       <button onClick={() => setCounter(0)}>Limpar contador</button>
     </div>
   )
