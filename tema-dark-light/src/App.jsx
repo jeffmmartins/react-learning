9eb6ef707955f7d1b18bf2b2e8736f417f72c6df
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-06-05 18:33:27 -0300

    atividade finalizada

--- tema-dark-light/src/App.jsx
+++ tema-dark-light/src/App.jsx
@@ -1,12 +1,37 @@
-import './App.css'
+import { useCallback, useState } from 'react'
+import { ThemeProvider } from 'styled-components';
+import { AppCOntainer, MainSection } from './components/app.style';
+import { Button } from './components/button.style';
+import { themes } from './themes';
+
 
 function App() {
-  
+  const [currentTheme, setCurrentTheme] = useState("light");
+
+  // memorizar uma função
+  const getOpositeTheme = useCallback(
+    () => (currentTheme === "light" ? "dark" : "light"),
+    [currentTheme]
+  );
+
+  const handleThemeSwitch = () => {
+    const newTheme = getOpositeTheme();
+    setCurrentTheme(newTheme);
+  };
 
   return (
-    <>
-      
-    </>
+    
+      <ThemeProvider theme={themes[currentTheme]}>
+        <AppCOntainer>
+          <MainSection>
+            <h1>You are in {currentTheme} mode</h1>
+            <Button onClick={handleThemeSwitch}>
+              switch to {getOpositeTheme()} mode 
+            </Button>
+          </MainSection>
+        </AppCOntainer>
+      </ThemeProvider>
+    
   )
 }
 
