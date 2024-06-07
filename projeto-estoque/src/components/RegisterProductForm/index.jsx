31587606b8a145c9b5a4f94258419908d949368e
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-06-07 11:34:28 -0300

    componente RegisterProductForm

---
+++ projeto-estoque/src/components/RegisterProductForm/index.jsx
@@ -0,0 +1,11 @@
+export const RegisterProductForm = () => {
+    return (
+        <form action="">
+            <input type="text" />
+            <input type="number" />
+            <select>
+                
+            </select>
+        </form>
+    )
+}
