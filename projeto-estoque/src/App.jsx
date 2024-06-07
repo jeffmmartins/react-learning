c7d36449fc67a4a5b823f1e9880977264978b315
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-06-07 09:33:16 -0300

    componente CardProduct

--- projeto-estoque/src/App.jsx
+++ projeto-estoque/src/App.jsx
@@ -1,53 +1,69 @@
-
-import './App.css'
-import { DefaultTemplate } from './components/template/DefaultTemplate';
+import "./App.css";
+import { CardProduct } from "./components/CradProduct";
+import { DefaultTemplate } from "./components/template/DefaultTemplate";
 
 function App() {
   const fruitList = [
     {
-       productName: "Banana",
-       productWeight: 20,
-       productCategory: "frutas",
+      productName: "Banana",
+      productWeight: 20,
+      productCategory: "frutas",
     },
     {
-       productName: "Morango",
-       productWeight: 5,
-       productCategory: "frutas",
+      productName: "Morango",
+      productWeight: 5,
+      productCategory: "frutas",
     },
     {
-       productName: "Uva",
-       productWeight: 12,
-       productCategory: "frutas",
+      productName: "Uva",
+      productWeight: 12,
+      productCategory: "frutas",
     },
     {
-       productName: "Maçã",
-       productWeight: 20,
-       productCategory: "frutas",
+      productName: "Maçã",
+      productWeight: 20,
+      productCategory: "frutas",
     },
- ];
+  ];
 
- const categories = [
+  const categories = [
     {
-       slug: "frutas",
-       label: "Frutas",
+      slug: "frutas",
+      label: "Frutas",
     },
     {
-       slug: "legumes",
-       label: "Legumes",
+      slug: "legumes",
+      label: "Legumes",
     },
     {
-       slug: "ovos",
-       label: "Ovos",
-    }
- ];
+      slug: "ovos",
+      label: "Ovos",
+    },
+  ];
 
   return (
     <>
       <DefaultTemplate>
-        <h1>Teste</h1>
+        <h1>Meu Estoque:</h1>
+        <ul>
+          {categories.map(({ label, slug }) => (
+            <li key={slug}>{label}</li>
+          ))}
+        </ul>
+        <ul>
+          {fruitList.map(({ productName, productWeight }) => {
+            return (
+              <CardProduct
+                key={productName}
+                productName={productName}
+                productWeight={productWeight}
+              />
+            );
+          })}
+        </ul>
       </DefaultTemplate>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
