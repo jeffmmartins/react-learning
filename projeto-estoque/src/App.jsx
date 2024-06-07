cb892632287ccb4eb0e96495db55aa78c3b251a1
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-06-07 08:45:33 -0300

    exercicio do projeto estoque

---
+++ projeto-estoque/src/App.jsx
@@ -0,0 +1,50 @@
+
+import './App.css'
+
+function App() {
+  const fruitList = [
+    {
+       productName: "Banana",
+       productWeight: 20,
+       productCategory: "frutas",
+    },
+    {
+       productName: "Morango",
+       productWeight: 5,
+       productCategory: "frutas",
+    },
+    {
+       productName: "Uva",
+       productWeight: 12,
+       productCategory: "frutas",
+    },
+    {
+       productName: "Maçã",
+       productWeight: 20,
+       productCategory: "frutas",
+    },
+ ];
+
+ const categories = [
+    {
+       slug: "frutas",
+       label: "Frutas",
+    },
+    {
+       slug: "legumes",
+       label: "Legumes",
+    },
+    {
+       slug: "ovos",
+       label: "Ovos",
+    }
+ ];
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
