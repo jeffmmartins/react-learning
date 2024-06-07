baf71d60cf151279262807baa4a59369b7f54ada
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-06-07 18:13:43 -0300

    projeto finalizado

--- projeto-estoque/src/components/RegisterProductForm/index.jsx
+++ projeto-estoque/src/components/RegisterProductForm/index.jsx
@@ -1,23 +1,33 @@
 import { useState } from "react"
 
 export const RegisterProductForm = ({categories}) => {
+    /*
     const [productName, setProductName] = useState("")
     const [productWeight, setProductWeight] = useState("")
     const [productCategory, setProductCategory] = useState("")
+    */
+
+    const [formData, setFormData] = useState({
+        productName: "",
+        productWeight: "",
+        productCategory: ""
+    })
 
     const onSubmit = (event) => {
         event.preventDefault()
-        console.log({productName,productWeight,productCategory})
-        setProductCategory("");
-        setProductName("");
-        setProductWeight("");
+        console.log(formData)
+        setFormData({
+            productName: "",
+            productWeight: "",
+            productCategory: ""
+        })
     }
 
     return (
         <form onSubmit={onSubmit}>
-            <input type="text" value={productName} onChange={(e) => setProductName(e.target.value) }/>
-            <input type="number" value={productWeight} onChange={(e) => setProductWeight(e.target.value)}/>
-            <select value={productCategory} onChange={(e)=> setProductCategory(e.target.value)}>
+            <input type="text" value={formData.productName} onChange={(e) => setFormData({...formData, productName: e.target.value}) }/>
+            <input type="number" value={formData.productWeight} onChange={(e) => setFormData({...formData, productWeight: e.target.value})}/>
+            <select value={FormData.productCategory} onChange={(e)=> setFormData({...formData, productCategory: e.target.value})}>
                 {categories.map(category=> (
                     <option key={category.slug} value={category.slug}> {category.label}</option>
                 ))}
