0a79c18b2eaec001bdf3b2b4352b9d7de91288fc
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-06-13 09:53:59 -0300

    useState criado

--- litsa-de-tarefas/src/cmponents/NewToDoForm.jsx
+++ litsa-de-tarefas/src/cmponents/NewToDoForm.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
 
 const NewToDoForm = () => {
+
+const {register, handleSubmit} = useForm()
+const [tarefas, setTarefas] = useState();
+
+const infoForm = (formData) => {
+    setTarefas(formData.tarefa) 
+}
+
   return (
     <div>
       <h1>Lista de Tarefas</h1>
-      <form>
-        <input type="text" />
+      {tarefas ? tarefas : null}
+      <form onSubmit={handleSubmit(infoForm)}>
+        <input type="text" placeholder='Exemplo: Estudar'{...register("tarefa")} />
         <button>Adicionar Tarefa</button>
       </form>
     </div>
