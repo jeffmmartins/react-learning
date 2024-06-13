988a7810f61648612213c246c057ac7cc8b6187d
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-06-13 08:37:40 -0300

    projeto lista de tarefas

---
+++ litsa-de-tarefas/src/cmponents/NewToDoForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NewToDoForm = () => {
+  return (
+    <div>
+      <h1>Lista de Tarefas</h1>
+      <form></form>
+    </div>
+  )
+}
+
+export default NewToDoForm
