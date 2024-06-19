f2816f83bbcb4401eb1e8d80c33c5f9df84eaa6c
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-06-19 17:26:05 -0300

    exemplo padrão

--- bootstrap_react/src/App.jsx
+++ bootstrap_react/src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
+
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <div className='card bg-dark text-white m-5'>
+      <div className='card-header'>Nootbook</div>
+      <div className='card-body'>
+        <h5 className='card-tittle'>2500</h5>
+        <p className='card-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+      </div>
+    </div>
   )
 }
 
