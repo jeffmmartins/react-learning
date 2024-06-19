2502a6aefd869348b22e61b7aa3b2293f178ba7e
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-06-19 17:33:09 -0300

    bootstrap como componente

--- bootstrap_react/src/App.jsx
+++ bootstrap_react/src/App.jsx
@@ -1,16 +1,22 @@
-
+import Card from 'react-bootstrap/Card'
+import CardHeader from 'react-bootstrap/esm/CardHeader'
 
 function App() {
   
 
   return (
-    <div className='card bg-dark text-white m-5'>
+    <>
+      <Card bg='dark' text='white' className='m-5'>
+        <CardHeader>Nootbook</CardHeader>
+      </Card>
+      <div className='card bg-dark text-white m-5'>
       <div className='card-header'>Nootbook</div>
       <div className='card-body'>
         <h5 className='card-tittle'>2500</h5>
         <p className='card-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
       </div>
     </div>
+    </>
   )
 }
 
