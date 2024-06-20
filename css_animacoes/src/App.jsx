4605d034abdc8a5a78355af27af22c74be6390ee
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-06-20 16:57:17 -0300

    animação criada

--- css_animacoes/src/App.jsx
+++ css_animacoes/src/App.jsx
@@ -1,13 +1,15 @@
 
+import { useState } from 'react'
 import './App.css'
 import Produto from './component/Produto'
 
 function App() {
-  
+  const [ativar, setAtivar] = useState(false)
 
   return (
     <>
-      <Produto/>
+      <button onClick={() => setAtivar(!ativar)}>Ativar</button>
+      {ativar && <Produto/>}
     </>
   )
 }
