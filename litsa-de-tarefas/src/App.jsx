1f128bd6212b64c4b8003eb25689f6ce774a4ec6
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-06-25 11:51:42 -0300

    intalando react icons

--- litsa-de-tarefas/src/App.jsx
+++ litsa-de-tarefas/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import NewToDoForm from "./cmponents/NewToDoForm";
+import Header from "./cmponents/Header";
 
 function App() {
   return (
     <>
+      <Header/>
       <NewToDoForm />
     </>
   );
---
+++ litsa-de-tarefas/src/cmponents/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>Lista de Tarefas</h1>
+    </div>
+  );
+};
+
+export default Header;
