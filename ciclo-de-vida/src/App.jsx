1d7f1a5e6249caae3a11fe6a78be99b4e2c9fcb4
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-08 17:58:38 -0300

    ciclo de vida: aplicando state e ternario no componente

--- ciclo-de-vida/src/App.jsx
+++ ciclo-de-vida/src/App.jsx
@@ -1,14 +1,23 @@
+import React from "react"
 
-import './App.css'
+class App extends React.Component {
+constructor() {
+  super();
+  this.state = {showCounter: false}
 
-function App() {
-  
+  }
 
-  return (
-    <>
-     
-    </>
-  )
+  render() {
+    return (
+      <>
+        <h1>Ciclo de vida</h1>
+        <button onClick={() => this.setState({showCounter: !this.state.showCounter})}>
+          {this.state.showCounter ? "remover Contador" : "mostrar  Contador"}</button>
+      </>
+    )
+  }
 }
+
+
 
 export default App
