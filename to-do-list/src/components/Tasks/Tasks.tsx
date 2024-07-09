86803784aabad39ce498d979a86cd2138d532f7a
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-09 16:25:59 -0300

    feat: aplicando o usestate no formulario

--- to-do-list/src/components/Tasks/Tasks.tsx
+++ to-do-list/src/components/Tasks/Tasks.tsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import style from "./style.module.scss";
 
 export const Tasks: React.FC = () => {
+    const [taskTitle, setTaskTitle] = useState("")
+
+
+
+
     return (
         <section className={style.container}>
-            <form>
-                <div>
+            {/*preventDefault evita o comportamento da página recarregar*/}
+            <form onSubmit={(event) => { event.preventDefault(); console.log(taskTitle) }}> 
+                <div >
                     <label htmlFor="task-title">Adicionar Tarefa</label>
-                    <input type="text" id="task-title" placeholder="Titulo da tarefa" />
+                    <input type="text" id="task-title" value={taskTitle} placeholder="Titulo da tarefa"
+                        onChange={(event) => { setTaskTitle(event.target.value) }} />
                 </div>
-                <button>Adicionar Tarefa</button>
+                <button type="submit">Adicionar Tarefa</button>
             </form>
         </section>
     )
