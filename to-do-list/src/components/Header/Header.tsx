3223301129cbecb435b0a5cbe7d3fed08a6332e7
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-09 10:45:25 -0300

    to do list(style): div container formatada

--- to-do-list/src/components/Header/Header.tsx
+++ to-do-list/src/components/Header/Header.tsx
@@ -1,15 +1,17 @@
 import style from "./style.module.scss"
 
 export const Header: React.FC = () => {
-  return (
-    <header className={style.header}>
-      <div>
-        <h1>My To do</h1>
-        <span>Bem Vindo, Jefferson</span>
-      </div>
-      <div>
-        {/*cards*/}
-      </div>
-    </header>
-  );
+    return (
+        <header className={style.header}>
+            <div className={style.container}>
+                <div>
+                    <h1>My To do</h1>
+                    <span>Bem Vindo, Jefferson</span>
+                </div>
+                <div>
+                    {/*cards*/}
+                </div>
+            </div>
+        </header>
+    );
 };
