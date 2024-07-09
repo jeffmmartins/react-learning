1817a2043b8185e254f575c949e539d7b758304f
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-09 18:40:10 -0300

    style: formatando as tasks

--- to-do-list/src/components/Tasks/Tasks.tsx
+++ to-do-list/src/components/Tasks/Tasks.tsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import style from "./style.module.scss";
 
 export const Tasks: React.FC = () => {
     const [taskTitle, setTaskTitle] = useState("")
 
 
-
+    const handleSubmitAddTask = (event: FormEvent) => {
+        event.preventDefault()
+    }
 
     return (
         <section className={style.container}>
             {/*preventDefault evita o comportamento da página recarregar*/}
-            <form onSubmit={(event) => { event.preventDefault(); console.log(taskTitle) }}> 
+            <form onSubmit={handleSubmitAddTask}> 
                 <div >
                     <label htmlFor="task-title">Adicionar Tarefa</label>
                     <input type="text" id="task-title" value={taskTitle} placeholder="Titulo da tarefa"
@@ -18,6 +20,16 @@ export const Tasks: React.FC = () => {
                 </div>
                 <button type="submit">Adicionar Tarefa</button>
             </form>
+            <ul>
+                <li>
+                    <input type="checkbox" id="task" />
+                    <label htmlFor="task">tarefa 1</label>
+                </li>
+                <li>
+                    <input type="checkbox" id="task" />
+                    <label htmlFor="task">tarefa 1</label>
+                </li>
+            </ul>
         </section>
     )
 }
