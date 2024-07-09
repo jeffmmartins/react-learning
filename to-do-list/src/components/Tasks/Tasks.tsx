5983fabf852feda239a4aad35d3f7ee29d403780
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-09 18:50:41 -0300

    feat: criando a lógica de adicionar tarefa

--- to-do-list/src/components/Tasks/Tasks.tsx
+++ to-do-list/src/components/Tasks/Tasks.tsx
@@ -2,11 +2,24 @@ import { FormEvent, useState } from "react";
 import style from "./style.module.scss";
 
 export const Tasks: React.FC = () => {
-    const [taskTitle, setTaskTitle] = useState("")
+    const [taskTitle, setTaskTitle] = useState("");
+    //para adicionar as tarefas é criado um array para adicionar as tarefas 
+    const [tasks, setTasks] = useState([])
 
+    /*
+        [
+            {title: tarefa 1}, done: boolean}
+        ]
+    */
 
+    // função disparada quando o usuario quer adicionar uma nova tarefa 
     const handleSubmitAddTask = (event: FormEvent) => {
         event.preventDefault()
+
+        if(taskTitle.length < 3) {
+            alert('Não é possivel adicionar uma tarefa com menos de 3 letras')
+            return
+        }
     }
 
     return (
