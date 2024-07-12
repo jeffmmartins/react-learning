eb26cbfc3a103b2cbffdd19ee5b559e062d9e2e6
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-12 18:51:00 -0300

    my-shop(header): importando os componentes estilizados

--- my-shop/src/components/Header.tsx
+++ my-shop/src/components/Header.tsx
@@ -2,11 +2,23 @@ import React from "react";
 import * as S from "./Style"
 
 export const Header: React.FC = () => {
-    return(
+    return (
         <>
             <S.StyledHeader>
-                <S.HeaderTitle>My Shop</S.HeaderTitle>
+                {/*Envolvendo tudo em uma div que foi nomeado de Wrapper*/}
+                <S.Wrapper>
+                    <S.HeaderTitle>My Shop</S.HeaderTitle>
+
+                    {/*Envolvendo os button em uma div que foi nomeado de buttonWrapper*/}
+                    <S.ButtonWrapper>
+                        <S.AutButton>Login</S.AutButton>
+
+                        <S.CartButton> Carrinho</S.CartButton>
+                    </S.ButtonWrapper>
+
+                </S.Wrapper>
             </S.StyledHeader>
+
         </>
     )
 }
--- my-shop/src/components/Style.ts
+++ my-shop/src/components/Style.ts
@@ -4,5 +4,12 @@ import styled from "styled-components";
 export const StyledHeader = styled.header`
 
 `
+export const Wrapper = styled.div``
 
 export const HeaderTitle = styled.h1``
+
+export const AutButton = styled.button``
+
+export const ButtonWrapper = styled.div``
+
+export const CartButton = styled.button``
