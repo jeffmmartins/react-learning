cede7a823331d97410e4b459a074d9b79f836538
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-12 21:03:19 -0300

    my-shop(style) : estilização do componente Header concluida

--- my-shop/src/components/Style.ts
+++ my-shop/src/components/Style.ts
@@ -2,14 +2,42 @@ import styled from "styled-components";
 
 // nomeando como styledHeader para nao gerar conflito
 export const StyledHeader = styled.header`
-
+background-color: navy;
 `
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
 
-export const HeaderTitle = styled.h1``
+max-width: 1240px;
+margin: 0 auto;
+padding: 0 2rem;
+height: 60px;
+`
 
-export const AutButton = styled.button``
+export const HeaderTitle = styled.h1`
+color: white;
+`
 
-export const ButtonWrapper = styled.div``
+export const AutButton = styled.button`
+border: none;
+border-radius: 5px;
+height: 30px;
+padding: 0 2rem;
+background-color: green;
+color: white;
+`
 
-export const CartButton = styled.button``
+export const ButtonWrapper = styled.div`
+display: flex;
+gap: 1rem;
+`
+
+export const CartButton = styled.button`
+border: none;
+border-radius: 5px;
+height: 30px;
+padding: 0 2rem;
+background-color: violet;
+color: white;
+`
