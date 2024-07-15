6d0a9422d3695578bbe4b0e917bb827f32eadf6b
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-14 22:57:32 -0300

    my-shop: incluido a função reducer para o carrinho

--- my-shop/src/redux/root.reducer.ts
+++ my-shop/src/redux/root.reducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux"
 import { userReducer } from "./UserReduce/reduce"
+import { cartReducer } from "./Cart/cart-reduce"
 
 // combinar todos reduces em um so
 export const rootReducer = combineReducers({
-    userReducer
+    userReducer,
+    cartReducer,
 })
 
 export type RootReducer = ReturnType<typeof rootReducer>
