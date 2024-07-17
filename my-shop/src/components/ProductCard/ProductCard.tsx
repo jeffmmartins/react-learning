c25780331748975aac9342f9a78779ce0a3d3992
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-17 11:24:56 -0300

    my-shop(feat): lógica para alternar o botão de remover para addicionar

--- my-shop/src/components/ProductCard/ProductCard.tsx
+++ my-shop/src/components/ProductCard/ProductCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import * as S from "./style"
 import { FiShoppingCart } from "react-icons/fi";
 import { Products } from "../../data/products";
-import { AiFillStar, AiOutlineStar} from "react-icons/ai"
+import { AiFillStar, AiOutlineStar } from "react-icons/ai"
 import { useDispatch, useSelector } from "react-redux";
 import { RootReducer } from "../../redux/root.reducer";
 
@@ -12,9 +12,13 @@ interface ProductCartProps {
 }
 
 // image -nao preciso fechar a tag , pois na pratica é como se fosse a tag img
-export const ProductCard: React.FC<ProductCartProps> = ({product}) => {
+export const ProductCard: React.FC<ProductCartProps> = ({ product }) => {
     const dispatch = useDispatch()
-    const {cart} = useSelector((rootReducer: RootReducer) => rootReducer.cartReducer)
+    const { cart } = useSelector((rootReducer: RootReducer) => rootReducer.cartReducer)
+
+    // encontrar o produto dentro do cart
+    // variavel boleana que está no carrinho. 
+    const isProductOnCart = cart.find((productOnCart) => product.id === productOnCart.id) !== undefined
 
     // ao clicar no botão adicionar é despachado a action. que no payload const o produto que foi adicionado 
     const handleAddProductToCart = () => {
@@ -24,9 +28,9 @@ export const ProductCard: React.FC<ProductCartProps> = ({product}) => {
             type: 'cart/add-product',
             payload: product,
         })
-    } 
+    }
 
-   
+
     return (
         <S.Card>
             <S.Image src={product.image} alt={product.description} />
@@ -36,13 +40,13 @@ export const ProductCard: React.FC<ProductCartProps> = ({product}) => {
 
             <S.ReviewPriceContainer>
                 <S.Review>
-                    {Array.from({length:5}).map(
-                        (_,index) => index < Math.round(product.rating.rate)
-                        ? (
-                            <AiFillStar key={index}/>
-                        ) : (
-                            <AiOutlineStar key={index}/>
-                        ))}
+                    {Array.from({ length: 5 }).map(
+                        (_, index) => index < Math.round(product.rating.rate)
+                            ? (
+                                <AiFillStar key={index} />
+                            ) : (
+                                <AiOutlineStar key={index} />
+                            ))}
                     ({` ${product.rating.rate}`})
                 </S.Review>
 
@@ -50,13 +54,13 @@ export const ProductCard: React.FC<ProductCartProps> = ({product}) => {
             </S.ReviewPriceContainer>
 
             <S.AddToCartButtonWrapper>
-                <S.AddToCartButton onClick={handleAddProductToCart }>
-                    Adicionar Ao Carrinho <FiShoppingCart />
-                </S.AddToCartButton>
-
-                <S.RemoveFromCartButton >
-                    Remover do Carrinho  <FiShoppingCart />
-                </S.RemoveFromCartButton>
+                {isProductOnCart ?
+                    <S.RemoveFromCartButton >
+                        Remover do Carrinho  <FiShoppingCart />
+                    </S.RemoveFromCartButton> :
+                    <S.AddToCartButton onClick={handleAddProductToCart}>
+                        Adicionar Ao Carrinho <FiShoppingCart />
+                    </S.AddToCartButton>}
             </S.AddToCartButtonWrapper>
 
         </S.Card>
