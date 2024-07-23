47fd9d4e86b07174099be174266bdf8638c5d435
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-23 10:06:40 -0300

    my-shop: criando o render para o componente cart

--- my-shop/src/components/Cart/Cart.test.tsx
+++ my-shop/src/components/Cart/Cart.test.tsx
@@ -1,5 +1,8 @@
+import { render } from "@testing-library/react"
+import { Cart } from "./Cart"
+
 describe("Cart > Unit Tests", () => {
     it("Should render an empty cart correctly", () => {
-
+        render(<Cart showCart={true}/>)
     })
 })
